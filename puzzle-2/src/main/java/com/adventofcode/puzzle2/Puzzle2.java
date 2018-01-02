baf5fa8ac2527bc79dde4a424e7cf0a1c31bb319
t.shaterov <teodor.shaterov@estafet.com>
2018-01-02 20:55:14 +0200

    Puzzle 2 project created

---
+++ puzzle-2/src/main/java/com/adventofcode/puzzle2/Puzzle2.java
@@ -0,0 +1,13 @@
+package com.adventofcode.puzzle2;
+
+/**
+ * Hello world!
+ *
+ */
+public class Puzzle2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
