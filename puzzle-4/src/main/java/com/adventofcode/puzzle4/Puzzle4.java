fc7632368eabf9125daf8ee8d1ee7ee2b3cdfe5b
t.shaterov <teodor.shaterov@estafet.com>
2018-01-04 10:01:48 +0200

    Puzzle4 project

---
+++ puzzle-4/src/main/java/com/adventofcode/puzzle4/Puzzle4.java
@@ -0,0 +1,11 @@
+package com.adventofcode.puzzle4;
+
+/**
+ * Hello world!
+ *
+ */
+public class Puzzle4 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
