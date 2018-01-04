62769bce3e650cfba487a1d72c82f5d30503ec5a
t.shaterov <teodor.shaterov@estafet.com>
2018-01-04 13:54:16 +0200

    Puzzle 5 project added

---
+++ puzzle-5/src/main/java/com/adventofcode/puzzle5/Puzzle5.java
@@ -0,0 +1,13 @@
+package com.adventofcode.puzzle5;
+
+/**
+ * Hello world!
+ *
+ */
+public class Puzzle5 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
