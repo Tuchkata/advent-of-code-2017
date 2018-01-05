bfd6a24fa614c6fedf737a4c6aab19411952345e
t.shaterov <teodor.shaterov@estafet.com>
2018-01-05 11:51:13 +0200

    Puzzle 5 solved

--- puzzle-5/src/main/java/com/adventofcode/puzzle5/Puzzle5.java
+++ puzzle-5/src/main/java/com/adventofcode/puzzle5/Puzzle5.java
@@ -1,13 +1,63 @@
 package com.adventofcode.puzzle5;
 
-/**
- * Hello world!
- *
- */
-public class Puzzle5 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Puzzle5 {
+  public static void main(String[] args) {
+    firstPart();
+    secondPart();
+  }
+
+  private static void firstPart() {
+    try {
+      List<Integer> steps = parseInput();
+      System.out.println(solve(steps, true));
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
     }
+  }
+
+  private static void secondPart() {
+    try {
+      List<Integer> steps = parseInput();
+      System.out.println(solve(steps, false));
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static int solve(List<Integer> steps, boolean isFirstPart) {
+    int count = 0;
+    int index = 0;
+    while (index >= 0 && index < steps.size()) {
+      int step = steps.get(index);
+      int increase = calculateIncreaseValue(isFirstPart, step);
+      steps.set(index, step + increase);
+      index += step;
+      count++;
+    }
+    return count;
+  }
+
+  private static int calculateIncreaseValue(boolean isFirstPart, int step) {
+    if (isFirstPart) {
+      return 1;
+    } else {
+      return step >= 3 ? -1 : 1;
+    }
+  }
+
+  private static List<Integer> parseInput() throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File("src/resources/input"));
+    List<Integer> steps = new ArrayList<Integer>();
+    while (scanner.hasNextLine()) {
+      steps.add(Integer.parseInt(scanner.nextLine()));
+    }
+    scanner.close();
+    return steps;
+  }
 }
